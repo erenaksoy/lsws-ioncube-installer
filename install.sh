8a02579b8ce68518fbd86ac66167bb7c009f7b5d
erenaksoy <17029657+erenaksoy@users.noreply.github.com>
2022-11-13 17:00:22 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#ioncube php extension installer for litespeed web servers
+wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar -zxvf ioncube_loaders_lin_x86-64.tar.gz 
+cp ioncube/ioncube_loader_lin_8.1.so /usr/local/lsws/lsphp81/lib/php/20210902
+rm ioncube_loaders_lin_x86-64.tar.gz
+rm -rf ioncube
+sudo echo "extension=ioncube_loader_lin_8.1.so" >> /usr/local/lsws/lsphp81/etc/php/8.1/mods-available/01-ioncube.ini
+sudo systemctl restart lsws
